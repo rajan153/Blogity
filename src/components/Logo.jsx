72c460824386c9463df08a4b82c8d956f9cd8cb8
Rajan <rajanmasih154@gmail.com>
2023-10-20 23:13:02 +0530

    First commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Logo({ width = "100px" }) {
+  return (
+    <div>
+      <img src="../src/assets/logo.png" width={70} />
+    </div>
+  );
+}
+
+export default Logo;
